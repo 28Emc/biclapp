42131c52c5e1ae3d1f0c410dc43360230a87defb
28Emc <edi@live.it>
2021-05-09 01:49:58 -0500

    mysql

--- src/main/java/com/biclapp/controller/BicicletasController.java
+++ src/main/java/com/biclapp/controller/BicicletasController.java
@@ -5,14 +5,12 @@ import com.biclapp.service.IBicicletasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api")
 public class BicicletasController {
 
     @Autowired
--- src/main/java/com/biclapp/controller/RutasController.java
+++ src/main/java/com/biclapp/controller/RutasController.java
@@ -5,14 +5,12 @@ import com.biclapp.service.IRutasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api")
 public class RutasController {
 
     @Autowired
