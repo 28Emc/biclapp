cb2ec738bf69111f2eab028c197216f2ab15f35b
28Emc <edi@live.it>
2021-05-13 21:31:43 -0500

    Fix Autoincrementing id accesorios 2

--- src/main/java/com/biclapp/model/entity/Accesorios.java
+++ src/main/java/com/biclapp/model/entity/Accesorios.java
@@ -2,12 +2,13 @@ package com.biclapp.model.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Accesorios {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nombre;
     private String descripcion;
